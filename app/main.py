cb041993c8fd093aa7cc0a0e1fc7ba4c3d916841
HassanMSh <hassan.m.shamseddine@gmail.com>
2025-09-13 05:39:14 +0300

    feat: ui enhancements

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from model.dots_ocr_4b import ocr_app
+# from model.dots_ocr_4b import ocr_app
 from app.ui import ui as ui_app  # make sure in ui.py you named it `ui = FastAPI()`
 
 # This is the FastAPI instance uvicorn will look for
 app = FastAPI()
 
 # # Mount the OCR API at /api
-app.mount("/api", ocr_app)
+# app.mount("/api", ocr_app)
 
 # Mount the UI at /
 app.mount("/", ui_app)
